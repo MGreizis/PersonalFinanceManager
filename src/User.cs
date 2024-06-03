624f480ff1905ad74b1162541d08a37aaa7ba382
Martins Greizis <grei0003@hz.nl>
2024-06-03 14:16:21 +0300

    hash stored passwords

--- src/User.cs
+++ src/User.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Security.Cryptography;
+using System.Text;
 using System.Text.Json;
 
 namespace PersonalFinanceManager
@@ -8,7 +10,8 @@ namespace PersonalFinanceManager
   public class User
   {
     public required string Username { get; set; }
-    public required string Password { get; set; }
+    public required string PasswordHash { get; set; }
+    public required string Salt { get; set; }
 
     private static string GetUserDataFilePath()
     {
@@ -25,10 +28,15 @@ namespace PersonalFinanceManager
       Console.WriteLine("Enter a password: ");
       string password = Console.ReadLine()!;
 
+      string salt = GenerateSalt();
+
+      string passwordHash = HashPassword(password, salt);
+
       User newUser = new User
       {
         Username = username,
-        Password = password
+        PasswordHash = passwordHash,
+        Salt = salt
       };
 
       SaveUser(newUser);
@@ -61,7 +69,26 @@ namespace PersonalFinanceManager
       {
         return new List<User>();
       }
-      return JsonSerializer.Deserialize<List<User>>(jsonString) ?? new List<User>();
+
+      var options = new JsonSerializerOptions
+      {
+        PropertyNameCaseInsensitive = true
+      };
+
+      var users = JsonSerializer.Deserialize<List<NullableUser>>(jsonString, options);
+
+      var finalUsers = new List<User>();
+      foreach (var nullableUser in users)
+      {
+        finalUsers.Add(new User
+        {
+          Username = nullableUser.Username ?? string.Empty,
+          PasswordHash = nullableUser.PasswordHash ?? string.Empty,
+          Salt = nullableUser.Salt ?? string.Empty
+        });
+      }
+
+      return finalUsers;
     }
 
     public static bool Login()
@@ -75,16 +102,54 @@ namespace PersonalFinanceManager
       List<User> users = LoadUsers();
       foreach (User user in users)
       {
-        if (user.Username == username && user.Password == password)
+        if (user.Username == username)
         {
-          Console.WriteLine("Login successful!");
-          MainMenu.DrawMainMenu();
-          return true;
+          string passwordHash = HashPassword(password, user.Salt);
+          if (user.PasswordHash == passwordHash)
+          {
+            Console.WriteLine("Login successful!");
+            MainMenu.DrawMainMenu();
+            return true;
+          }
+          break;
         }
       }
 
       Console.WriteLine("Invalid username or password. Please try again.");
       return false;
+    }
+
+    private static string GenerateSalt()
+    {
+      byte[] saltBytes = new byte[16];
+      using (var rng = RandomNumberGenerator.Create())
+      {
+        rng.GetBytes(saltBytes);
+      }
+      return Convert.ToBase64String(saltBytes);
+    }
+
+    private static string HashPassword(string password, string salt)
+    {
+      byte[] saltBytes = Convert.FromBase64String(salt);
+      byte[] passwordBytes = Encoding.UTF8.GetBytes(password);
+      byte[] passwordWithSaltBytes = new byte[saltBytes.Length + passwordBytes.Length];
+
+      Buffer.BlockCopy(saltBytes, 0, passwordWithSaltBytes, 0, saltBytes.Length);
+      Buffer.BlockCopy(passwordBytes, 0, passwordWithSaltBytes, saltBytes.Length, passwordBytes.Length);
+
+      using (var sha256 = SHA256.Create())
+      {
+        byte[] hashBytes = sha256.ComputeHash(passwordWithSaltBytes);
+        return Convert.ToBase64String(hashBytes);
+      }
+    }
+
+    private class NullableUser
+    {
+      public string? Username { get; set; }
+      public string? PasswordHash { get; set; }
+      public string? Salt { get; set; }
     }
   }
 }
